ddbe7584ba390fe0fc917814c6cfcd912a5f9a94
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-04 12:36:29 +0530

    feat:Player Starting Position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,7 +6,10 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            int PositionOfPlayer = 0;
+
             Console.WriteLine("welcome to Snake Ladder Program");
+            Console.WriteLine("Position of player at initial is " + PositionOfPlayer);
         }
     }
 }
